7ebd96c24ad8eefc062dc535f7b1e0b9384d71b7
TrungTran2751999 <trantrung2751999@gmail.com>
2023-02-11 12:55:09 +0700

    thay doi file customer

--- sapo-product/src/main/java/vn/sapo/purchaseOrderItem/PurchaseOrderItemService.java
+++
@@ -1,5 +0,0 @@
-package vn.sapo.purchaseOrderItem;
-
-public interface PurchaseOrderItemService {
-    int getQuantityPurchaseByProductIdAndOrderStatusCode(Integer productId, String orderStatusCode);
-}
